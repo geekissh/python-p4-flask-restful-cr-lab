9720a6eaeffe460da26ff994bca7c59f046c6878
Mark Kelsall <markkelsall1995@gmail.com>
2024-04-15 19:24:57 +0300

    finale

--- server/models.py
+++ server/models.py
@@ -7,3 +7,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
